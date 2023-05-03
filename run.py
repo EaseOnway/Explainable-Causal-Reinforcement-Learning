a935e53bf9ea37ef7feaeeb1e66466a71cf4f17b
EaseOnway <yu.zhongwei@qq.com>
2023-05-03 15:24:21 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from alg import Experiment
 
 
-Experiment.run(["fitting", "aimtest", 
-                "--seed=1", "--train-size=20000", "--aim=0.1", "--n-batch=50000",
-                "--test-size=5000", "--explore=1", "--n-step=1", '--ablation=dense'])
+Experiment.run()
+
+# For measuring AIM accuracy.
+# Experiment.run(["fitting", "aimtest", 
+#                 "--seed=1", "--train-size=20000", "--aim=0.1", "--n-batch=50000",
+#                 "--test-size=5000", "--explore=1", "--n-step=1", '--ablation=dense'])
